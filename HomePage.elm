569ab9bd16618940e76a8904d04406e6677e3467
Joey Scott <joannascott29@hotmail.co.uk>
2017-06-07 16:53:05 +0100

    Build view for contraceptive types Relates #11

--- HomePage.elm
+++ HomePage.elm
@@ -19,5 +19,39 @@ view model =
         ]
 
 
+
+--contraceptiveTypes : Model -> Html Msg
+
+
+contraceptiveTypes model =
+    div [ class "screen-container" ]
+        [ elmHubHeader
+        , div
+            [ class "half-screen half-screen--non-hormonal" ]
+            [ button
+                [ class "half-screen--button" ]
+                [ text "Non-hormonal" ]
+            ]
+        , div [ class "half-screen half-screen--hormonal" ]
+            [ button
+                [ class "half-screen--button" ]
+                [ text "Hormonal" ]
+            , p [] [ text "Contraceptive methods that utilise hormones to prevent pregnancy" ]
+            ]
+        ]
+
+
+elmHubHeader =
+    nav [ class "nav-container" ]
+        [ div [ class "nav-container__image" ]
+            [ img [ class "nav-image", src "http://via.placeholder.com/50x50" ] []
+            ]
+        , div [ class "nav-tracker-button" ]
+            [ a [ class "nav-tracker-button__link", href "#" ]
+                [ text "My tracker" ]
+            ]
+        ]
+
+
 main =
-    view "homepage"
+    contraceptiveTypes "homepage"
